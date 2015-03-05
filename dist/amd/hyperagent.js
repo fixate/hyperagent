c1b8f226a9576ce954a28d4cd4890dae80d09fe3
Stan Bondi <stan@fixate.it>
2015-03-05 15:27:33 +0200

    Export EmbeddedResource

--- dist/amd/hyperagent.js
+++ dist/amd/hyperagent.js
@@ -5,6 +5,7 @@ define("/hyperagent",
     var Resource = __dependency1__.Resource;
     var LazyResource = __dependency1__.LazyResource;
     var LinkResource = __dependency1__.LinkResource;
+    var EmbeddedResource = __dependency1__.EmbeddedResource;
     var Properties = __dependency2__.Properties;
     var CurieStore = __dependency3__.CurieStore;
     var _config = __dependency4__.config;
@@ -19,6 +20,7 @@ define("/hyperagent",
     __exports__.LazyResource = LazyResource;
     __exports__.LinkResource = LinkResource;
     __exports__.CurieStore = CurieStore;
+    __exports__.EmbeddedResource = EmbeddedResource;
     __exports__.configure = configure;
     __exports__._config = _config;
   });
--- dist/hyperagent.js
+++ dist/hyperagent.js
@@ -39,6 +39,7 @@ define("hyperagent",
     var Resource = __dependency1__.Resource;
     var LazyResource = __dependency1__.LazyResource;
     var LinkResource = __dependency1__.LinkResource;
+    var EmbeddedResource = __dependency1__.EmbeddedResource;
     var Properties = __dependency2__.Properties;
     var CurieStore = __dependency3__.CurieStore;
     var _config = __dependency4__.config;
@@ -53,6 +54,7 @@ define("hyperagent",
     __exports__.LazyResource = LazyResource;
     __exports__.LinkResource = LinkResource;
     __exports__.CurieStore = CurieStore;
+    __exports__.EmbeddedResource = EmbeddedResource;
     __exports__.configure = configure;
     __exports__._config = _config;
   });
